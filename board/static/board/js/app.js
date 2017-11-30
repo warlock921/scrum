59caf919669abb290e5bdb7ce394d2d47760fd06
warlock921 <caoyu921@163.com>
2017-12-01 00:30:55 +0800

    页面显示部分还有点小问题

---
+++ board/static/board/js/app.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ * @authors Your Name (you@example.org)
+ * @date    2017-11-28 21:21:55
+ * @version $Id$
+ */
+
+var app = (function($){
+	var config = $('#config'),
+		app = JSON.parse(config.text());
+
+	$(document).ready(function(){
+		var router = new app.router();
+	});
+	return app;
+})(jQuery);
---
+++ board/static/board/js/models.js
@@ -0,0 +1,76 @@
+/**
+ * 
+ * @authors Your Name (you@example.org)
+ * @date    2017-11-28 20:03:35
+ * @version $Id$
+ */
+
+(function ($,Backbone,_,app){
+	function csrfSafeMethod(method){
+		return(/^(GET|HEAD|OPTIONS|TRACE)$/i.test(method));
+	}
+
+	function getCookie(name){
+		var cookieValue = null;
+		if(document.cookie && document.cookie!=''){
+			var cookies = document.cookie.split(';');
+			for (var i=0;i<cookies.length;i++){
+				var cookie = $.trim(cookies[i]);
+				if(cookie.substring(0,name.length+1) == (name+'=')){
+					cookieValue = decodeURIComponet(
+						cookie.substring(name.length+1));
+					break;
+				}
+			}
+		}
+		return cookieValue;
+	}
+
+	$.ajaxPrefilter(function(settings,originalOptions,xhr){
+		var csrftoken;
+		if(!csrfSafeMethod(settings.type) && !this.crossDomain){
+			csrftoken=getCookie('csrftoken');
+			xhr.setRequestHeader('X-CSRFToken',csrftoken);
+		}
+	});
+	var Session = Backbone.Model.extend({
+		default:{
+			token:null
+		},
+		initialize:function(options){
+			this.options = options;
+			$.ajaxPrefilter($.proxy(this._setupAuth,this));
+			this.load();
+		},
+		load:function(){
+			var token = localStorage.apiToken;
+			if(token){
+				this.set('token',token);
+			}
+		},
+		save:function(token){
+			this.set('token',token);
+			if(token===null){
+				localStorage.removeItem('apiToken');
+			}else{
+				localStorage.apiToken=token;
+			}
+		},
+		delete:function(){
+			this.save(null);
+		},
+		authenticated:function(){
+			return this.get('token') !== null;
+		},
+		_setupAuth: function(settings,originalOptions,xhr){
+			if(this.authenticated()){
+				xhr.setRequestHeader(
+					'Authorization',
+					'Token ' + this.get('token')
+				);
+			}
+		}
+	});
+
+	app.session = new Session();
+})(jQuery,Backbone,_,app)
