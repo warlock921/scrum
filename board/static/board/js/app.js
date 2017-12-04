04df34f67a6c18c672fd922352b7e1db66e2ab36
warlock921 <caoyu921@163.com>
2017-12-04 20:18:31 +0800

    完成代码-但有错

--- board/static/board/js/app.js
+++ board/static/board/js/app.js
@@ -5,12 +5,13 @@
  * @version $Id$
  */
 
-var app = (function($){
-	var config = $('#config'),
-		app = JSON.parse(config.text());
-
-	$(document).ready(function(){
-		var router = new app.router();
-	});
-	return app;
+var app = (function ($) {
+    var config = $('#config'),
+        app = JSON.parse(config.text());
+    
+    $(document).ready(function () {
+    	var router = new app.router();
+    });
+        
+    return app;
 })(jQuery);
